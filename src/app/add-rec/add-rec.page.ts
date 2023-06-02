4a913fa6275a299e5cb3247d5768090a0820db44
franpvp <franpvp.98@gmail.com>
2023-06-01 22:40:14 -0400

    Se agrega temporizador

--- src/app/add-rec/add-rec.page.ts
+++ src/app/add-rec/add-rec.page.ts
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRecPage implements OnInit {
 
-  constructor() { }
+  tiempoRestante: string;
+  temporizador: any;
+  tiempoIngresado: number;
+  dias: number;
+
+  datosTemp = {
+    nombreMedicamento: ''
+  }
+
+  constructor() { 
+    this.tiempoRestante = '';
+    this.tiempoIngresado = 0;
+    this.dias = 0;
+  }
 
   ngOnInit() {
+  }
+
+  iniciarTemporizador() {
+    if (this.temporizador) {
+      clearInterval(this.temporizador);
+    }
+
+    const horaActual = new Date();
+    const tiempoFinalizacion = new Date(horaActual.getTime() + this.tiempoIngresado * 60 * 60 * 1000);
+
+    this.temporizador = setInterval(() => {
+      const ahora = new Date();
+      if (ahora < tiempoFinalizacion) {
+        this.tiempoRestante = this.obtenerTiempoRestante(tiempoFinalizacion, ahora);
+      } else {
+        this.recordatorio();
+        clearInterval(this.temporizador);
+      }
+    }, 1000);
+  }
+
+  obtenerTiempoRestante(tiempoFinal: Date, tiempoActual: Date): string {
+    const diferencia = tiempoFinal.getTime() - tiempoActual.getTime();
+    const horas = Math.floor(diferencia / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+
+    return horas + ' horas ' + minutos + ' minutos';
+  }
+
+  recordatorio() {
+    console.log('¡Es hora del recordatorio!');
   }
 
 }
