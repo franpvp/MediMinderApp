7689199c2ad5545079da2eb09dda437727fc7853
franpvp <franpvp.98@gmail.com>
2023-06-04 23:28:25 -0400

    Se eliminan algunos modulos

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -9,7 +9,7 @@ import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
 // Componentes Angular Material
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
